7a691702ccf15956a96a7889d9aff569c1c222eb
Subeen Chung <subeenchung@hotmail.com>
2017-11-07 09:58:23 -0500

    test version

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"os"
+	"os/signal"
+	"os/exec"
+	"io/ioutil"
+	"strings"
+	"syscall"
 
-	"github.com/shirou/gopsutil/mem"
-	"gopkg.in/yaml.v2"
+	//"github.com/shirou/gopsutil/process"
+	"gopkg.in/yaml.v1"
 )
 
 type config struct {
-	UsagiProspector []struct {
-		Name string `yaml:"name"`
-		Path string `yaml:"path"`
-		Param string `yaml:"param"`
-	} `yaml:"UsagiProspector"`
+	UsagiProspector []Prospector `yaml:"usagiprospector"`
+}
+
+type Prospector struct {
+	Name string `yaml:"name"`
+	Path string `yaml:"path"`
+	Param string `yaml:"param"`
 }
 
 func checkBinaryExists(b string) bool {
@@ -35,13 +44,85 @@ func readConfig() config {
 	if err != nil {
 		fmt.Println("error opening config file")
 	}
-	err = yaml.Unmarshal(f, c)
+	err = yaml.Unmarshal(f, &c)
 	if err != nil {
-		fmt.Println("error unmarshalling config file")
+		fmt.Println("error unmarshalling config file", err)
 	}
 	return c
 }
 
+func checkIfUp(n string) bool {
+	cmd := strings.Replace("ps ux | awk '/PROCESS/ && !/awk/ {print $2}'", "PROCESS", n, -1)
+	fmt.Println(cmd)
+	out, err := exec.Command("sh", "-c", cmd).Output()
+	if err != nil {
+			fmt.Println(err)
+	}
+	if len(out) < 1 {
+		return false
+	}
+
+	return true
+}
+
+func start(n, args string) {
+	binary, err := exec.LookPath(n)
+	if err != nil {
+		panic(err)
+	}
+	env := os.Environ()
+	exec := syscall.Exec(binary, strings.Split(args, " "), env)
+	if exec != nil {
+		panic(exec)
+	}
+}
+
 func main(){
+
 	fmt.Println("Starting USAGI - Zombifying Daemon")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	shutdown := false
+
+	cfg := readConfig()
+
+	/* for _, v := range c.UsagiProspector {
+		fmt.Printf("name: %s\n", v.Name)
+		fmt.Printf("path: %s\n", v.Path)
+		fmt.Printf("param: %s\n", v.Param)
+	} */
+
+	OUTTER:
+	for {
+		if shutdown {
+			break
+		}
+
+		select {
+		case s, ok := <- c:
+			if ok {
+				fmt.Println("\nUSAGI is shutting down...\n")
+				fmt.Println(fmt.Sprintf("\n\nReceived signal: %x\n\n", s))
+				//shutdown sequence started
+				shutdown = true
+				//close go channel for os signal
+				c = nil
+				continue OUTTER
+			}
+		default:
+			for _, v := range cfg.UsagiProspector {
+				run := checkIfUp(v.Path)
+				if !run {
+					//Start process since its not running
+					go start(v.Path, v.Param)
+				}
+			}
+			//finished checking, go to sleep
+			time.Sleep(10000 * time.Millisecond)
+		}
+	}
+
+	fmt.Println("USAGI has shutdown gracefully. Sayonara!")
 }
